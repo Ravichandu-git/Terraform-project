648f7ae97eaf9b3f09bb72e4950e18663f3b8924
ravichandu-git <ravib29282@gmail.com>
2026-07-27 19:06:25 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+module "ec2" {
+  source = "./modules/ec2"
+
+  instance_name = "demo-ec2"
+  ami_id         = "ami-09d88f7c4c272b0c5" # Amazon Linux 2023 (example)
+  instance_type  = "t3.micro"
+}
+
+module "s3" {
+  source = "./modules/s3"
+
+  bucket_name = "my-unique-demo-bucket-123456789"
+}
